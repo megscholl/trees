b2b65374c278900a4d421f92683dd3acd53a429d
Meg Scholl <meghan.e.scholl@gmail.com>
2018-02-02 15:45:16 -0600

    worked through the functions for loop to create the tree and include spaces to give it shape

--- js/main.js
+++ js/main.js
@@ -8,30 +8,24 @@ let pineValues = {
     character: treeCharacter
 };
 
+
 document.getElementById("growTree").addEventListener("click", function(e) {
    e.preventDefault();
+
    console.log("Tree Height: ", pineValues.height.value);
    console.log("Tree Character: ", pineValues.character.value);
-   let userHeight = pineValues.height.value;
-   let userCharacter = pineValues.character.value;
 
-    if (userHeight == "" || userCharacter == "") {
+    if (pineValues.height.value == "" ||    pineValues.character.value == "") {
         alert('Please enter a height and character');
     };
-});
 
+    
+    var treeGrow = (pineValues.height.value * 2) -1; //
+     for (var i = 1; i <= treeGrow; i += 2){
+         
+         var space = " ";
+         let space2 = space.repeat(pineValues.height.value - i / 2);
+         let grow = (pineValues.character.value).repeat(i);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log(space2, grow);
+}});
